2b9b862b82401e97a640048ef2adabf53490ac92
Oleksii Sheiko <programmer.alex97@gmail.com>
2021-03-24 23:32:22 +0200

    Transform basic html templates into Vue components and added route for each

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    meta: {
+      layout: 'main'
+    },
     component: Home,
   },
   {
@@ -19,12 +22,60 @@ const routes = [
     component: () => import('../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta:{
+      layout: 'empty'
+    },
+    component: () => import('../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'login',
     meta: {
       layout: 'main'
     },
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/history/detail-record',
+    name: 'detail-record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Record.vue')
   },
 ];
 
